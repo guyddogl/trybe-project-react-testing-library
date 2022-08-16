7d53bd85361efe784cb824987a9d6b40d6a42d43
Guyddo <guyddogl@gmail.com>
2022-08-16 17:58:20 -0300

    Remove renderWithRouter from file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
-
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{component}</Router>),
-    history,
-  });
-};
 
 describe('Teste o componente <App.js />', () => {
   test('Se existem os links Home, About e Favorite Pokémons', () => {
