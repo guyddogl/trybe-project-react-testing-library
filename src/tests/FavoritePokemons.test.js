251919415b6673e7e2810aa5600c5154bb8b61f7
Guyddo <guyddogl@gmail.com>
2022-08-16 16:00:38 -0300

    Requirement 3. Test component FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Se existe um h2 com o texto About Pokédex', () => {
+    render(<FavoritePokemons />);
+    const pageTitle = screen
+      .getByRole('heading', { level: 2, name: /Favorite pokémons/i });
+    expect(pageTitle).toBeInTheDocument();
+  });
+  test('Se existe um parágrafo com o texto No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
