7267a672504381d475496b8435e98f931f1d8125
Guyddo <guyddogl@gmail.com>
2022-08-16 16:06:18 -0300

    Requirement 4. Test component NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Se existe um h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const pageTitle = screen.getByRole('heading', { level: 2, name: /Page requested/i });
+    expect(pageTitle).toBeInTheDocument();
+  });
+  test('Se existe a imagem da Pokédex', () => {
+    render(<NotFound />);
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgNotFound = screen.getByAltText(/Pikachu crying because the page requested/i);
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound.getAttribute('src')).toBe(imgSrc);
+  });
+});
